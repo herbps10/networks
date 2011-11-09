461eff8f0d0d8633bac67f7ffd6f7298006752fe
Herb Susmann <herbps10@gmail.com>
2011-11-09 20:47:30 +0000

    Added ruby run script

---
+++ run_network.rb
@@ -0,0 +1,9 @@
+t = 0
+while t < 1
+	command = "/home/herb/git/networks/networks " + t.to_s + " " + (t + 0.001).to_s + " 0.001"
+
+	exec command
+
+	t += 0.001
+	t = (t * 10000).round.to_f / 10000.0
+end
